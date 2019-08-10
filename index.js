a1e3809c5a0a7e0ca3815a84f680add7c8d98721
ARINZE OGBONNA <arinze.ogbonna.198717@unn.edu.ng>
2019-08-10 19:31:40 +0100

    Lemme Host it and SEE

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use("/api/courses", coursesRoute);
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/dist"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
