7f9969401f1874b7265248f72854bf70b2e57007
ARINZE OGBONNA <arinze.ogbonna.198717@unn.edu.ng>
2019-08-10 19:31:04 +0100

    Lemme Host it and SEE

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 const port = process.env.PORT || 7000;
